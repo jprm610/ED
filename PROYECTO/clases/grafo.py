aa216d94832e6873b514ba4cf42b6d9daf108077
jprm610 <jprm610@gmail.com>
2023-11-27 14:51:11 -0500

    4.3

--- PROYECTO/clases/grafo.py
+++ PROYECTO/clases/grafo.py
@@ -3,15 +3,68 @@ import numpy as np
 class Grafo :
     def __init__(self, V:list) -> None:
         self.V = sorted(V)
-        self.matrix = np.zeros((len(self.V), len(self.V)), dtype=tuple)
+        self.matrixKm = np.zeros((len(self.V), len(self.V)), dtype=int)
+        self.matrixMinutos = np.zeros((len(self.V), len(self.V)), dtype=int)
 
     def agregarArista(self, A, B, KM, MINUTOS) :
         AIndex = self.V.index(A)
         BIndex = self.V.index(B)
-        self.matrix[AIndex, BIndex] = (KM, MINUTOS)
-        self.matrix[BIndex, AIndex] = (KM, MINUTOS)
+
+        # Agregar en matrixKm
+        self.matrixKm[AIndex, BIndex] = KM
+        self.matrixKm[BIndex, AIndex] = KM
+
+        # Agregar en matrixMinutos
+        self.matrixMinutos[AIndex, BIndex] = MINUTOS
+        self.matrixMinutos[BIndex, AIndex] = MINUTOS
 
     def checkCarretera(self, A, B) :
         AIndex = self.V.index(A)
         BIndex = self.V.index(B)
-        return isinstance(self.matrix[AIndex, BIndex], tuple)
+        return not self.matrixKm[AIndex, BIndex] == 0
+    
+    def dijkstra(self, inicio, destino, modo='distancia') :
+        # Verificar si los vértices dados existen en el grafo
+        if inicio not in self.V or destino not in self.V :
+            raise ValueError("Los vértices de inicio o destino no existen en el grafo.")
+
+        # Seleccionar la matriz de pesos según el modo
+        if modo == 'distancia' :
+            matrix_peso = self.matrixKm
+        elif modo == 'tiempo' :
+            matrix_peso = self.matrixMinutos
+        else:
+            raise ValueError("Modo no válido. Debe ser 'distancia' o 'tiempo'.")
+
+        # Inicializar acumulador de pesos, conjunto de vértices visitados y un diccionario para almacenar los padres
+        pesos = {v: float('inf') for v in self.V}
+        pesos[inicio] = 0
+        visitados = set()
+        padres = {v: None for v in self.V}
+
+        while len(visitados) < len(self.V) :
+            # Seleccionar el vértice no visitado con la distancia mínima
+            actual = min(set(pesos.keys()) - visitados, key=lambda v: pesos[v])
+
+            # Marcar el vértice como visitado
+            visitados.add(actual)
+
+            # Actualizar las pesos y los padres a los vértices adyacentes
+            for vecino in self.V:
+                if (self.checkCarretera(actual, vecino) and vecino not in visitados) :
+                    peso = matrix_peso[self.V.index(actual), self.V.index(vecino)]
+                    distancia_total = pesos[actual] + peso
+                    if distancia_total < pesos[vecino] :
+                        pesos[vecino] = distancia_total
+                        padres[vecino] = actual
+
+        # Reconstruir el camino desde el destino hasta el inicio
+        camino = [destino]
+        while padres[destino] is not None :
+            destino = padres[destino]
+            camino.insert(0, destino)
+
+        # Tiempo o distancia total del viaje
+        total = pesos[camino[-1]]
+
+        return camino, total
--- PROYECTO/main.py
+++ PROYECTO/main.py
@@ -10,12 +10,12 @@ def main() :
     while True :
         print('FUNCIONALIDADES')
         print('1) Existencia de carretera entre A y B')
-        print('2) Carretera más corta entre A y B (Kilómetros)')
-        print('3) Carretera más corta entre A y B (Minutos)')
+        print('2) Camino más corta entre A y B (Kilómetros)')
+        print('3) Camino más corta entre A y B (Minutos)')
         entrada = int(input('Elija una funcionalidad o 0 para terminar: '))
         if entrada == 1 : func1(G)
-        elif entrada == 2 : pass
-        elif entrada == 3 : pass
+        elif entrada == 2 : func2(G)
+        elif entrada == 3 : func3(G)
         else : break
     print('Programa terminado.')
     return
@@ -43,12 +43,32 @@ def func1(G: Grafo) :
     print('1) Check carretera entre A y B')
     A = input('Elija ciudad A: ')
     B = input('Elija ciudad B: ')
-    if A not in G.V or B not in G.V : raise Exception('Verifique las ciudades ingresadas')
+    if A not in G.V or B not in G.V : raise ValueError('Verifique las ciudades ingresadas')
 
-    hayCarretera = G.checkCarretera(A, B)
+    hayCarretera = G.checkCarretera(A=A, B=B)
     if hayCarretera : print('Sí hay carretera')
     else : print('No hay carretera')
 
     return
+
+def func2(G: Grafo) :
+    print("2) Camino más corta entre A y B (Kilómetros)")
+    A = input('Elija ciudad A: ')
+    B = input('Elija ciudad B: ')
+    if A not in G.V or B not in G.V : raise ValueError('Verifique las ciudades ingresadas')
+    camino, distancia = G.dijkstra(inicio=A, destino=B, modo='distancia')
+    print('El camino más corto por distancia es: ', end='')
+    print(*camino, sep=' -> ', end='')
+    print(f' con una distancia total de {distancia} kilómetros.')
+
+def func3(G: Grafo) :
+    print("3) Camino más corta entre A y B (Minutos)")
+    A = input('Elija ciudad A: ')
+    B = input('Elija ciudad B: ')
+    if A not in G.V or B not in G.V : raise ValueError('Verifique las ciudades ingresadas')
+    camino, tiempo = G.dijkstra(inicio=A, destino=B, modo='tiempo')
+    print('El camino más corto por tiempo es: ', end='')
+    print(*camino, sep=' -> ', end='')
+    print(f' con un tiempo total de {tiempo} minutos.')
 
 main()
